24d0bcf3a391ec75938ae869b5588540334430ff
Riley C <collins.e.riley@gmail.com>
2021-12-09 21:33:18 -0600

    remove webhooks for netlify and ghost, update reset css

--- .eleventy.js
+++ .eleventy.js
@@ -3,20 +3,4 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
 };
 
-const ghostContentAPI = require("@tryghost/content-api");
 
-const api = new ghostContentAPI ({
-  url: process.env.GHOST_API_URL,
-  key: process.env.GHOST_CONTENT_API_KEY,
-  version:"v3"
-});
-
-config.addCollection("tags", async function(collection) {
-  collection = await api.pages
-    .browse({
-      include: "tags",
-      limit: "all"
-    })
-    .catch((err) => {
-      console.error(err);
-    });
