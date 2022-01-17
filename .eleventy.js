51769990c88571814691dd17afbec028319a3f96
Riley C <collins.e.riley@gmail.com>
2022-01-17 15:59:28 -0600

    i git commit -m "start ghost and begin pagination"

--- .eleventy.js
+++ .eleventy.js
@@ -15,61 +15,64 @@ const stripDomain = url => {
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addCollection("docs", async function(collection) {
-  collection = await api.pages
-    .browse({
-      include: "authors",
-      limit: "all"
-    })
-    .catch(err => {
-      console.error(err);
-    });
+//   eleventyConfig.addCollection("posts", async function(collection) {
+//   collection = await api.posts
+//     .browse({
+//       include: "authors",
+//       limit: "all"
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     });
 
-  collection.map(doc => {
-    doc.url = stripDomain(doc.url);
-    doc.primary_author.url = stripDomain(doc.primary_author.url);
+//     console.log(collection)
 
-    // Convert publish date into a Date object
-    doc.published_at = new Date(doc.published_at);
-    return doc;
-  });
 
-  return collection;
-});
+//   collection.forEach(doc => {
+//       doc.url = stripDomain(doc.url);
+//       doc.primary_author.url = stripDomain(doc.primary_author.url);
+
+//     // Convert publish date into a Date object
+//     doc.published_at = new Date(doc.published_at);
+//     return doc;
+//   });
+
+//   return collection;
+// });
 
   // Get all tags
-  eleventyConfig.addCollection("tags", async function(collection) {
-    collection = await api.tags
-      .browse({
-        include: "count.posts",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
+  // eleventyConfig.addCollection("tags", async function(collection) {
+  //   collection = await api.tags
+  //     .browse({
+  //       include: "count.posts",
+  //       limit: "all"
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
 
     // Get all posts with their tags attached
-    const posts = await api.posts
-      .browse({
-        include: "tags,authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
+  //   const posts = await api.posts
+  //     .browse({
+  //       include: "tags,authors",
+  //       limit: "all"
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
 
-    // Attach posts to their respective tags
-    collection.forEach(async tag => {
-      const taggedPosts = posts.filter(post => {
-        post.url = stripDomain(post.url);
-        return post.primary_tag && post.primary_tag.slug === tag.slug;
-      });
-      if (taggedPosts.length) tag.posts = taggedPosts;
+  //   // Attach posts to their respective tags
+  //   collection.forEach(async tag => {
+  //     const taggedPosts = posts.filter(post => {
+  //       post.url = stripDomain(post.url);
+  //       return post.primary_tag && post.primary_tag.slug === tag.slug;
+  //     });
+  //     if (taggedPosts.length) tag.posts = taggedPosts;
 
-      tag.url = stripDomain(tag.url);
-    });
+  //     tag.url = stripDomain(tag.url);
+  //   });
 
-    return collection;
-  });
+  //   return collection;
+  // });
 
 };
